fced425a3af7912cf7384a1e395113a8eb6d644a
dmitry <dimon199689@gmail.com>
2020-09-12 14:47:11 +0300

    добавлены геттеры

--- src/main/java/Security.java
+++ src/main/java/Security.java
@@ -2,10 +2,29 @@ import java.util.Date;
 
 public class Security {
 
-    public int id;
-    public Date endDate; //String?
-    public String codeCurrency;
-    public String cade;
-    public int value;
+    private int id;
+    private String endDate; //String?
+    private String codeСurrency;
+    private String code;
+    private long value;
 
+    public int getId() {
+        return id;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getCodeСurrency() {
+        return codeСurrency;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public long getValue() {
+        return value;
+    }
 }
