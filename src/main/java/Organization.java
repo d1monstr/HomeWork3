5bc57f496dfab6fae8709ee87be9f4b1ad5f709f
dmitry <dimon199689@gmail.com>
2020-09-12 12:33:53 +0300

    Создан класс организации

--- src/main/java/Organization.java
+++ src/main/java/Organization.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class Organization {
+    public String organizationName;
+    public String address;
+    public List<String> phones;
+    public String inn;
+    public String ogrn;
+    public List<Security> securities;
+
 }
