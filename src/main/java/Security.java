fbd70f6d7c93f21eefd5f767982952e4fa194adc
dmitry <dimon199689@gmail.com>
2020-09-12 12:33:13 +0300

    Создан класс ценных бумаг

--- src/main/java/Security.java
+++ src/main/java/Security.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Date;
 
 public class Security {
+
+    public int id;
+    public Date endDate; //String?
+    public String codeCurrency;
+    public String cade;
+    public int value;
+
 }
